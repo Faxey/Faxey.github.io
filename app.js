63934aeb868cb7732dd1d0b98dc6d6aa373c98c5
Faxey <74048122+Faxey@users.noreply.github.com>
2023-09-01 16:51:34 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent('auto-play-video', {
-    schema: {
-      video: {type: 'string'},
-    },
-    init() {
-      const v = document.querySelector(this.data.video)
-      v.play()
-    },
-  })
